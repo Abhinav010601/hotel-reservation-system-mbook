8f5552a536a6919aebc46cf214e987c434de674b
Abhinav Singla <abhinavsingla18@gmail.com>
2025-02-23 00:42:06 +0530

    initial commit- MBook assignment

---
+++ src/main/java/com/metalbook_backendAssignment/MBookAssessmentApplication/MBookAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.metalbook_backendAssignment.MBookAssessmentApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBookAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MBookAssessmentApplication.class, args);
+	}
+
+}
